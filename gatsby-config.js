8fc61c4d481da10120fb309e5aa1ba2c0ea228eb
Lit Phansiri <lphansiri@gmail.com>
2023-08-01 14:05:29 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-theme-tut`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
